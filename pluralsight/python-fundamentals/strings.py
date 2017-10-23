becf96b726f17e2d1a0e7d2067f439264b3037ae
Steven Barclay <barclays@familysearch.org>
2017-10-23 15:34:28 -0600

    Adding files for exploration.
    These are for exploration of Strings and Collections.

---
+++ pluralsight/python-fundamentals/strings.py
@@ -0,0 +1,12 @@
+
+print("This is a string")
+print("This is another string")
+
+# Working with string functions
+
+name = "steven"
+print("Original String: " + name)
+print("Capitalized String: " + name.capitalize())
+print("Is the string alpha-numeric: " + str(name.isalnum()))
+print("Number of 'e's in the string: " + str(name.count('e')))
+print("Length of the string: " + str(name.__len__()))
