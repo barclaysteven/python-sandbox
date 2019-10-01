b035fa678a2ac36c6dd735b9523992506bc42c2f
Steven Barclay <barclays@familysearch.org>
2019-10-01 16:20:51 -0600

    Update files for policy validation
    Adding additional concepts for parsing a CFN template and then running
    the extracted policy through the custom simulator using a test configuration

--- aws/read-cfn/read-cfn-template.py
+++ aws/read-cfn/read-cfn-template.py
@@ -1,65 +1,81 @@
 import yaml
+import boto3
+import json
 
-test_policy = "test-policy.yaml"
+TEST_CONFIG = "test-config.yaml"
+TEST_ACCOUNT_ID = "221453998677"
 
 
 class POC:
     def __init__(self):
         print("Inside __init__")
+        session = boto3.Session(profile_name="play-fh2")
+        self.iam_client = session.client('iam', region_name='us-east-1')
 
     def main(self):
-        template = yaml.load(open(test_policy), Loader=yaml.FullLoader)
-        print(template)
-        resources = template['Resources']
+        test_config = yaml.load(open(TEST_CONFIG), Loader=yaml.FullLoader)
 
-        for resource in resources:
-            properties = resources.get(resource)['Properties']
-            policies = properties['Policies']
-            for policy in policies:
-                policy_doc = policy['PolicyDocument']
-                print(policy_doc)
-        # policies = yaml.load(open('policies.yaml'), Loader=yaml.FullLoader)
-        # for policy in policies['policies']:
-        #     print(policy['policyName'])
-        #     actions = policy['actions']
-        #     print(actions)
-        #     permission = policy['permission']
-        #     print(permission)
-        #     resources = policy['resources']
-        #     if 'context' in policy:
-        #         context = policy['context']
-        #         key_name = context['keyName']
-        #         context_type = context['type']
-        #         value = context['value']
-        #         response = self.iam.simulate_principal_policy(
-        #             PolicySourceArn=source_policy_arn,
-        #             ActionNames=actions,
-        #             ResourceArns=resources,
-        #             ContextEntries=[
-        #                 {
-        #                     'ContextKeyName': key_name,
-        #                     'ContextKeyValues': [value],
-        #                     'ContextKeyType': context_type
-        #                 }
-        #             ]
-        #         )
-        #     else:
-        #         response = self.iam.simulate_principal_policy(
-        #             PolicySourceArn=source_policy_arn,
-        #             ActionNames=actions,
-        #             ResourceArns=resources
-        #             )
-        #     evaluation_results = response['EvaluationResults']
-        #     for evaluation_result in evaluation_results:
-        #         action = evaluation_result['EvalActionName']
-        #         decision = evaluation_result['EvalDecision']
-        #         resource_name = evaluation_result['EvalResourceName']
-        #         if decision != permission:
-        #             print(action + ' has result of ' + decision + ' but wanted ' + permission + ' for '
-        #                   + resource_name + '::Fail')
-        #         else:
-        #             print(action + ' has result of ' + decision + ' for ' + resource_name + '::Pass')
-        #     print()
+        for test_cases in test_config['TestCases']:
+            modified_file = "mod." + test_cases['TemplateName']
+
+            with open(test_cases['TemplateName'], 'r') as infile, \
+                 open(modified_file, 'w') as outfile:
+                data = infile.read()
+                data = data.replace("!Sub", '').replace("${AWS::AccountId}", TEST_ACCOUNT_ID)
+                outfile.write(data)
+
+            template = yaml.load(open(modified_file), Loader=yaml.FullLoader)
+
+            for assertion in test_cases['PolicyAssertions']:
+                test_policy_name = assertion['PolicyName']
+                for resource in template['Resources']:
+                    properties = template['Resources'].get(resource)['Properties']
+                    managed_policy_name = properties['ManagedPolicyName']
+                    if managed_policy_name == test_policy_name:
+                        policy_doc = properties['PolicyDocument']
+                        break
+                for test in assertion['Tests']:
+                    test_name = test['Name']
+                    actions = test['Actions']
+                    expected_decision = test['ExpectedDecision']
+                    test_resources = test['Resources']
+
+                    print("Executing test: " + test_name)
+                    if "Context" in test:
+                        key_name = test['Context']['KeyName']
+                        value = test['Context']['Value']
+                        context_type = test['Context']['Type']
+                        response = self.iam_client.simulate_custom_policy(
+                            PolicyInputList=[json.dumps(policy_doc)],
+                            ActionNames=actions,
+                            ResourceArns=test_resources,
+                            ContextEntries=[
+                                {
+                                    'ContextKeyName': key_name,
+                                    'ContextKeyValues': [value],
+                                    'ContextKeyType': context_type
+                                }
+                            ]
+                        )
+                    else:
+                        response = self.iam_client.simulate_custom_policy(
+                            PolicyInputList=[json.dumps(policy_doc)],
+                            ActionNames=actions,
+                            ResourceArns=test_resources
+                        )
+
+                    evaluation_results = response['EvaluationResults']
+                    # print(evaluation_results)
+                    for evaluation_result in evaluation_results:
+                        action = evaluation_result['EvalActionName']
+                        decision = evaluation_result['EvalDecision']
+                        resource_name = evaluation_result['EvalResourceName']
+                        if decision != expected_decision:
+                            print(action + ' has result of ' + decision + ' but wanted ' + expected_decision + ' for '
+                                  + resource_name + '::Fail')
+                        else:
+                            print(action + ' has result of ' + decision + ' for ' + resource_name + '::Pass')
+                    print()
 
 
 if __name__ == '__main__':
